74c8a1355286c927d937420f58fb52d277956af6
kayu55 <iwanles109@gmail.com>
2025-09-02 16:18:01 +0700

    Add files via upload

--- wspro.sh
+++ wspro.sh
@@ -40,29 +40,5 @@ systemctl enable ws.service
 systemctl start ws.service
 systemctl restart ws.service
 
-# Installing Service
-cat > /etc/systemd/system/ws-ovpn.service << END
-[Unit]
-Description=OpenVPN
-Documentation=https://google.com
-After=network.target nss-lookup.target
-
-[Service]
-Type=simple
-User=root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-ExecStart=/usr/bin/python3 -O $wspro/ws.py 10012
-Restart=on-failure
-
-[Install]
-WantedBy=multi-user.target
-END
-
-systemctl daemon-reload
-systemctl enable ws-ovpn
-systemctl start ws-ovpn
-systemctl restart ws-ovpn
 
 rm -f $0
