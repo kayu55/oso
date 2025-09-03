0568dac3bafccfe7cc422cbdc9ead5a4cc4f085b
kayu55 <iwanles109@gmail.com>
2025-09-03 19:00:15 +0700

    Update ws.py

--- ws.py
+++ ws.py
@@ -18,7 +18,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:109'
-RESPONSE = (b'HTTP/1.1 101 <font color="green">Direct Blitar Pusat</b></font>\r\n'
+RESPONSE = (b'HTTP/1.1 101 <font color="green">ORDER WA 083851335795</b></font>\r\n'
             b'Upgrade: websocket\r\n'
             b'Connection: Upgrade\r\n'
             b'Sec-WebSocket-Accept: foo\r\n\r\n')
